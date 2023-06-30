c28d56a2b76dbd40652d50290ab85a0d5ecc8bd3
Joschi <joschi.grey@posteo.de>
2023-06-30 17:09:08 +0200

    Initial

---
+++ ChummerDB/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ChummerDB;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ ChummerDB/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace ChummerDB;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
